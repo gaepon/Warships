d1a0d2811360a13b531c0ebb3a60f21db7d2e663
Jules Rousseaux <jrssx04@gmail.com>
2024-09-18 10:41:43 +0200

    Completed game loop and bataille.py, began work on joueur.py

--- bataille.py
+++ bataille.py
@@ -2,9 +2,26 @@ import joueur as jr
 
 class Bataille:
 	def __init__(self, j1, j2):
-		self.j1=j1
-		self.j2=j2
-	
-	def joue(self, position, joueur):
-		
-	
+		self.js=[j1, j2]
+        self.turn=0
+
+    def tour(self):
+        j=self.js[self.turn%2]
+        pos=j.get_coor()
+        res=self.js[(self.turn+1)%2].attack(coor)
+        if res==0:
+            print("Rate !\n")
+        elif res==1:
+            print("Touche !\n")
+        elif:
+            print("Coule !\n")
+        else:
+            print("Coule !\n")
+            return self.end(j)
+        j.getShots().affiche()
+        self.turn+=1
+        return 1
+
+    def end(self, victor):
+        print(f"{j.getName()} a gagne")
+        return 0
--- joueur.py
+++ joueur.py
@@ -1,21 +1,29 @@
 import grid as gd
 
 class Joueur:
-	def __init__(self):
-		self.grille, self.b_list=gd.genere_grille()
+    def __init__(self, name):
+        self.grille, self.b_list=gd.genere_grille()
+        self.shots=[[0 for _ in range(10)] for ᄑ in range(10)]
+        self.name=name
 	
-	def get_coor(self):
-		pass
+    def getCoor(self):
+        pass
+
+    def getShots(self):
+        pass
+
+    def getName(self):
+        return self.name
 	
 class Human(Joueur):
-	def __init__(self):
-		super().__init__()
-		#print(self.grille)
+    def __init__(self, name):
+        super().__init__(name)
+        #print(self.grille)
 	
-	def get_coor(self):
-		x=int(input("x : "))
-		y=int(input("y : "))
-		return x, y
+    def get_coor(self):
+        x=int(input("x : "))
+        y=int(input("y : "))
+        return x, y
 
 #a=Human()
 #print(a.get_coor())
