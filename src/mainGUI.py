7a2b699fc88591e46fa7952bd071c96dece5046d
Jules Rousseaux <jrssx04@gmail.com>
2024-10-02 12:02:08 +0200

    Continued work on GUI, game is now fully playable with GUI but only between two humans.
    Next things :
     - Finish 'Hit', 'Miss', 'Sunk' screen
      - Add an end

--- src/mainGUI.py
+++ src/mainGUI.py
@@ -1,5 +1,14 @@
+import joueur as pl
+import ui
+import sys
+from PySide6.QtWidgets import QApplication
+
 def main():
-    pass
+    j=[pl.Human("Joueur1"), pl.Human("Joueur2")]
+    app=QApplication()
+    w=ui.GUI(j)
+    w.show()
+    sys.exit(app.exec())
 
 if __name__=="__main__":
     main()
--- src/ui.py
+++ src/ui.py
@@ -1,5 +1,6 @@
 import sys
 import grid as gd
+import numpy as np
 import joueur as pl
 from PySide6.QtGui import QColor
 from PySide6.QtWidgets import QApplication, QWidget, QGridLayout, QMainWindow, QVBoxLayout, QLabel, QPushButton
@@ -12,35 +13,39 @@ def hex_to_color(code):
     return QColor.fromRgb(rgb[0], rgb[1], rgb[2])
 
 class GridCase(QWidget):
-    def __init__(self, x, y, mainWindow, clickable):
+    def __init__(self, x:int, y:int, mainWindow, clickable:bool):
         super().__init__()
         self.x=x
         self.y=y
         self.clickable=clickable
         self.main=mainWindow
     
-    def mousePressEvent(self, QMouseEvent):
+    def mousePressEvent(self, QMouseEvent) -> None:
         if self.underMouse() and self.clickable:
             w=self.parentWidget()
             if w:
-                w.setGridCase(self.x, self.y, -1)
-                color=w.getColorList()
-                newColor=hex_to_color(color[0])
-                p=self.palette()
-                p.setColor(self.backgroundRole(), newColor)
-                self.setPalette(p)
-                self.main.nextWidget()
+                self.main.processTurn([self.x, self.y])
+    
+    def updateCase(self) -> None:
+        w=self.parentWidget()
+        if w:
+            color=w.getColorList()
+            newColor=hex_to_color(color[self.parentWidget().getGridCase(self.x, self.y)+1])
+            p=self.palette()
+            p.setColor(self.backgroundRole(), newColor)
+            self.setPalette(p)
+            self.update()
 
 class GridWidget(QWidget):
-    def __init__(self, grid, mainWindow, clickable=True):
+    def __init__(self, grid:list[list[int]], mainWindow, clickable:bool=True):
         super().__init__()
         self.grille=grid
         self.setFixedSize(590,590)
         self.color = ["#050505","#101044","#F1C40F","#2ECC71","#F39C12","#E74C3C","#5B2C6F"]
-        l = QGridLayout()
+        self.l = QGridLayout()
         
-        l.setVerticalSpacing(1)
-        l.setHorizontalSpacing(1)
+        self.l.setVerticalSpacing(1)
+        self.l.setHorizontalSpacing(1)
 
         for i in range(10):
             for j in range(10):
@@ -50,37 +55,50 @@ class GridWidget(QWidget):
                 i_p = gc.palette()
                 i_p.setColor(gc.backgroundRole(), set_color)
                 gc.setPalette(i_p)
-                l.addWidget(gc, i, j)
+                self.l.addWidget(gc, i, j)
         
-        self.setLayout(l)
+        self.setLayout(self.l)
     
-    def getColorList(self):
+    def getColorList(self) -> list[str]:
         return self.color
     
-    def setGridCase(self, x, y, val):
-        self.grille[y][x]=val
+    def getGridCase(self, x:int, y:int) -> int:
+        return self.grille[y][x]
+    
+    def updateGrid(self, ng):
+        self.grille=ng
+
+        for i in range(10):
+            for j in range(10):
+                self.l.itemAtPosition(i, j).widget().updateCase()
 
 class TurnScreen(QWidget):
-	def __init__(self, text, p=None):
-		super().__init__(parent=p)
-		self.text=text
+    def __init__(self, text:str, p:QWidget=None):
+        super().__init__(parent=p)
+        self.text=text
+        self.label=QLabel(parent=self)
 		
-		l=QVBoxLayout(self)
-		t=QLabel(parent=self)
-		t.setText(self.text)
-		t.setAlignment(Qt.AlignCenter)
-		b=QPushButton("Next", parent=self)
-		b.clicked.connect(self.parentWidget().nextWidget)
+        l=QVBoxLayout(self)
+        self.label.setText(self.text)
+        self.label.setAlignment(Qt.AlignCenter)
+        b=QPushButton("Next", parent=self)
+        b.clicked.connect(self.parentWidget().nextWidget)
 		
-		l.addWidget(t)
-		l.addWidget(b)
-		self.setLayout(l)
+        l.addWidget(self.label)
+        l.addWidget(b)
+        self.setLayout(l)
+    
+    def setText(self, text:str) -> None:
+        self.label.setText(text)
 
 class DualGrid(QWidget):
-    def __init__(self, player, p=None):
+    def __init__(self, player:pl.Joueur, mainWindow:QMainWindow, p=None):
         super().__init__(parent=p)
+        self.p=player
         shotGrid = player.getShots()
         boatGrid = player.getGrid()
+        self.grid1 = GridWidget(boatGrid, mainWindow, False)
+        self.grid2 = GridWidget(shotGrid, mainWindow)
         
         t1=QLabel()
         t2=QLabel()
@@ -91,54 +109,85 @@ class DualGrid(QWidget):
         l=QGridLayout()
         l.addWidget(t1, 0, 0)
         l.addWidget(t2, 0, 1)
-        l.addWidget(GridWidget(boatGrid, self.parentWidget(), False), 1, 0)
-        l.addWidget(GridWidget(shotGrid, self.parentWidget()), 1, 1)
+        l.addWidget(self.grid1, 1, 0)
+        l.addWidget(self.grid2, 1, 1)
         
         self.setLayout(l)
+    
+    def updateAll(self) -> None:
+        self.grid1.updateGrid(self.p.getGrid())
+        self.grid2.updateGrid(self.p.getShots())
+    
 
 class Screen(QWidget):
-    def __init__(self, j1, j2, p=None):
+    def __init__(self, j1:pl.Joueur, j2:pl.Joueur, p=None):
         super().__init__(parent=p)
         
         self.i=0
         
-        self.g1 = DualGrid(j1, p=self)
+        self.g1 = DualGrid(j1, p, p=self)
         self.ps1 = TurnScreen(j1.getName(), p=self)
-        self.g2 = DualGrid(j2, p=self)
+        self.st1 = TurnScreen("", p=self)
+        self.g2 = DualGrid(j2, p, p=self)
         self.ps2 = TurnScreen(j2.getName(), p=self)
+        self.st2 = TurnScreen("", p=self)
         
         self.ps1.resize(1230, 620)
         self.ps2.resize(1230, 620)
 
         self.g1.hide()
         self.g2.hide()
+        self.st1.hide()
+        self.st2.hide()
         self.ps2.hide()
         self.ps1.show()
 
     @Slot()
-    def nextWidget(self):
+    def nextWidget(self) -> None:
         self.i+=1
-        widList=[self.ps1, self.g1, self.ps2, self.g2]
+        widList=[self.ps1, self.g1, self.st1, self.ps2, self.g2, self.st2]
         widList[self.i%len(widList)].show()
         for y in range(len(widList)):
             if y!=self.i%len(widList):
                 widList[y%len(widList)].hide()
+    
+    def updateAll(self) -> None:
+        self.g1.updateAll()
+        self.g2.updateAll()
+        self.update()
 
 class GUI(QMainWindow):
-    def __init__(self, j1, j2):
+    def __init__(self, j:list[pl.Joueur]):
         super().__init__()
         self.setFixedSize(1230,620)
         self.setWindowTitle("WARSHIPS !")
-        
-        w=Screen(j1, j2, p=self)
-    
-        self.setCentralWidget(w)
+        self.turn=0
+        self.j=j
 
+        self.w=Screen(j[0], j[1], p=self)
+    
+        self.setCentralWidget(self.w)
+    
+    def processTurn(self, coord:list[int]) -> None:
+        j1=self.j[self.turn%2]
+        j2=self.j[(self.turn+1)%2]
+        res=j2.attack(coord, j1)
+        if res==0:
+            pass
+        elif res==1:
+            pass
+        elif res==2:
+            pass
+        else:
+            pass
+        self.w.updateAll()
+        self.w.nextWidget()
+        self.turn+=1
 
 if __name__=="__main__":
     app=QApplication()
     j1=pl.Human("J1")
     j2=pl.Human("J2")
-    w=GUI(j1, j2)
+    w=GUI([j1, j2])
     w.show()
     sys.exit(app.exec())
